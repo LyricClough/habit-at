6bb043ae2eae85684f139caac58eed423cbd399e
Aaditya Yanamandra <aaya7809@colorado.edu>
2025-04-10 14:04:38 -0600

    settings backend

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -2,5 +2,10 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(10),
+    email_notif BOOLEAN DEFAULT TRUE,
+    phone_notif BOOLEAN DEFAULT FALSE,
+    show_profile BOOLEAN DEFAULT FALSE,
+    dark_mode BOOLEAN DEFAULT FALSE
 );
