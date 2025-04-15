d467d86b4e4ca9ed3e0918ad7d7fcc6430fc519b
Cal Duffy <dedu4158@colorado.edu>
2025-04-15 14:25:15 -0600

    Updated Calendar Branch With Current Main

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,20 +1,47 @@
 CREATE TABLE users (
-  user_id SERIAL PRIMARY KEY,
+  user_id SERIAL PRIMARY KEY NOT NULL,
   username VARCHAR(100) NOT NULL UNIQUE,
   email VARCHAR(100) NOT NULL UNIQUE,
   password VARCHAR(100) NOT NULL
+  phone VARCHAR(10),
+  email_notif BOOLEAN DEFAULT TRUE,
+  phone_notif BOOLEAN DEFAULT FALSE,
+  show_profile BOOLEAN DEFAULT FALSE,
+  dark_mode BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE habits (
-  user_habit_id SERIAL PRIMARY KEY,
-  habit_name VARCHAR(100) NOT NULL,
-  description TEXT,
+  habit_id SERIAL PRIMARY KEY NOT NULL,
+  habit_name VARCHAR(20) NOT NULL,
+  Description VARCHAR(200),
   weekday INT NOT NULL CHECK (weekday >= 0 AND weekday <= 6),
-  time_slot INT NOT NULL CHECK (time_slot >= 0 AND time_slot < 24)
+  time_slot INT NOT NULL CHECK (time_slot >= 0 AND time_slot < 24),
+  counter INT UNSIGNED DEFAULT 0
 );
 
 CREATE TABLE users_to_habits (
-  user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
-  user_habit_id INT REFERENCES habits(user_habit_id) ON DELETE CASCADE,
-  PRIMARY KEY (user_id, user_habit_id)
+  user_id INT NOT NULL,
+  user_habit_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+ 	FOREIGN KEY (habit_id) REFERENCES habits (habit_id) ON DELETE CASCADE,
+);
+
+CREATE TABLE history (
+  history_id SERIAL PRIMARY KEY NOT NULL,
+  date DATE NOT NULL
+);
+
+CREATE TABLE habits_to_history (
+	habit_id INT NOT NULL,
+  history_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+ 	FOREIGN KEY (habit_id) REFERENCES habits (habit_id) ON DELETE CASCADE,
+);
+
+CREATE TABLE friends (
+ 	Sender INT NOT NULL,
+ 	Receiver INT NOT NULL,
+ 	Mutual BOOLEAN DEFAULT FALSE,
+ 	FOREIGN KEY (sender) REFERENCES users (user_id) ON DELETE CASCADE,
+  FOREIGN KEY (receiver) REFERENCES users (user_id) ON DELETE CASCADE
 );
