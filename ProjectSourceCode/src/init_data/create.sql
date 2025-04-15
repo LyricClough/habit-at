5939c152661009b37ed4ef11f4dd5fabf5f7466b
Cal Duffy <dedu4158@colorado.edu>
2025-04-15 14:54:09 -0600

    Fixed Create.sql so that db is actually created

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
   user_id SERIAL PRIMARY KEY NOT NULL,
   username VARCHAR(100) NOT NULL UNIQUE,
   email VARCHAR(100) NOT NULL UNIQUE,
-  password VARCHAR(100) NOT NULL
+  password VARCHAR(100) NOT NULL,
   phone VARCHAR(10),
   email_notif BOOLEAN DEFAULT TRUE,
   phone_notif BOOLEAN DEFAULT FALSE,
@@ -16,14 +16,14 @@ CREATE TABLE habits (
   Description VARCHAR(200),
   weekday INT NOT NULL CHECK (weekday >= 0 AND weekday <= 6),
   time_slot INT NOT NULL CHECK (time_slot >= 0 AND time_slot < 24),
-  counter INT UNSIGNED DEFAULT 0
+  counter INT DEFAULT 0
 );
 
 CREATE TABLE users_to_habits (
   user_id INT NOT NULL,
-  user_habit_id INT NOT NULL,
+  habit_id INT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
- 	FOREIGN KEY (habit_id) REFERENCES habits (habit_id) ON DELETE CASCADE,
+ 	FOREIGN KEY (habit_id) REFERENCES habits (habit_id) ON DELETE CASCADE
 );
 
 CREATE TABLE history (
@@ -34,8 +34,8 @@ CREATE TABLE history (
 CREATE TABLE habits_to_history (
 	habit_id INT NOT NULL,
   history_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
  	FOREIGN KEY (habit_id) REFERENCES habits (habit_id) ON DELETE CASCADE,
+  FOREIGN KEY (history_id) REFERENCES history (history_id) ON DELETE CASCADE
 );
 
 CREATE TABLE friends (
